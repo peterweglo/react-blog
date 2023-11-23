8a11cb51762d308f4230b83bfc23897280c339a3
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-11-23 11:17:54 +0100

    Add post deletion

--- src/components/pages/PostPage/PostPage.js
+++ src/components/pages/PostPage/PostPage.js
@@ -1,19 +1,30 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getPostById } from '../../../redux/postsRedux';
 import { Navigate } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+import { removePost } from '../../../redux/postsRedux';
 
 const PostPage = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const { id } = useParams();
+  const dispatch = useDispatch();
+  const handleRemove = () => {
+    dispatch(removePost(id));
+  };
+
   const postData = useSelector((state) => getPostById(state, id));
-  if (!postData) return <Navigate to='/' />;
+  if (!postData) return <Navigate to="/" />;
 
   return (
-    <main className='d-flex justify-content-center'>
-      <div className='pe-2 me-2'>
-        <Card className='mb-4 border border-0'>
+    <main className="d-flex justify-content-center">
+      <div className="pe-5 me-5">
+        <Card className="mb-4 border border-0">
           <Card.Body>
             <Card.Title>{postData.title}</Card.Title>
             <Card.Text>
@@ -26,23 +37,47 @@ const PostPage = () => {
           </Card.Body>
         </Card>
       </div>
-      <div className='ps-2 ms-2'>
+      <div className="ps-5 ms-5">
         <Button
-          variant='outline-info'
+          variant="outline-info"
           as={Link}
           to={`/post/edit/${postData.id}`}
-          className='m-1'
+          className="m-1"
         >
           Edit
         </Button>
-        <Button
-          variant='outline-danger'
-          as={Link}
-          to={`/post/edit/${postData.id}`}
-          className='m-1'
-        >
+        <Button variant="outline-danger" className="m-1" onClick={handleShow}>
           Delete
         </Button>
+      </div>
+      <div>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Are you sure?</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>
+              This operation will completely remove this post from the app!
+              <br />
+              Are you sure you want to do that?
+            </p>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button variant="danger" onClick={handleRemove}>
+              Remove
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </main>
   );
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -5,10 +5,16 @@ export const getPostById = ({ posts }, id) =>
 
 //actions
 const createActionName = (actionName) => `app/posts/${actionName}`;
+const REMOVE_POST = createActionName('REMOVE_POST');
 
 //action creators
+
+export const removePost = (payload) => ({ type: REMOVE_POST, payload });
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case REMOVE_POST:
+      return statePart.filter((post) => post.id !== action.payload);
     default:
       return statePart;
   }
